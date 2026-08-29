d971b2e16e984ba150d8d54e3b81ba428e03c57f
Hiroto Yoshida <jum6308@gmail.com>
2026-08-29 22:42:39 +0900

    revised a bit

--- app/components/education/CourseTimeline.tsx
+++ app/components/education/CourseTimeline.tsx
@@ -263,7 +263,7 @@ export default function CourseTimeline() {
           <p className="text-base text-foreground/80 leading-normal text-[var(--foreground)]/80">
             ・Oct 2025 - Feb 2026: SAP Japan (Support Engineer, Internship) <br />
             ・Aug 2025 - Sep 2025: Microsoft Japan (Technical Support Engineer, Internship)<br />
-            ・Apr 2025 - July 2025: And AI Co., Ltd. (Flutter Engineer, Internship)<br />
+            ・Apr 2025 - July 2025: & AI (Flutter Engineer, Internship)<br />
             ・Mar 2021 - Mar 2022: Starbucks Coffee Japan (Barista, Part time job)
           </p>
         </div>
@@ -277,7 +277,7 @@ export default function CourseTimeline() {
             Certifications
           </h2>
           <p className="text-base text-foreground/80 leading-normal text-[var(--foreground)]/80">
-            ・Sep 2025: Microsoft Certified: Security, Compliance, and Identity Fundamentals <br />
+            ・Sep 2025: Microsoft Certified: Security, Compliance, and Identity Fundamentals (SC-900) <br />
             ・Jul 2025: Fundamental Information Technology Engineer Examination (基本情報技術者試験) <br />
             ・Jun 2024: TOEIC Listening & Reading - 945 <br />
             ・Nov 2022: TOEFL iBT - 84
